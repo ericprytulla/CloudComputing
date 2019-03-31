4be1816d1f2e7f709d75422cd4490ecd6820f770
EricPrytulla <eric.prytulla@dibuco.de>
2019-03-31 20:47:20 +0200

    ...

--- cloud-chat/src/app/chat/chat.component.ts
+++ cloud-chat/src/app/chat/chat.component.ts
@@ -2,13 +2,60 @@ import { Component, OnInit } from '@angular/core';
 import {SocketService} from "../services/socket.service";
 
 export class Message{
-  public sender: string;
-  public message: string;
-  public to: string;
-  constructor(message: string, to: string, sender?: string){
+  readonly sender: string;
+  readonly message: string;
+  readonly to: string;
+  readonly timeStamp: Date;
+  constructor(message: string, to: string, timeStamp: Date, sender?: string){
     this.message = message;
     this.sender = sender;
     this.to = to;
+    this.timeStamp = timeStamp;
+  }
+
+  get timeStampString(): string{
+    return this.timeStamp.toLocaleString();
+  }
+}
+
+export class Chatroom{
+  readonly name: string;
+  readonly id: string;
+  readonly type: string;
+  public messages: Message[] = [];
+  public users: User[] = [];
+  public last_msg: Date;
+
+  constructor(name, id, type: string){
+    this.id = id;
+    this.name = name;
+    this.type = type;
+  }
+
+  pushMessage(message: Message){
+    this.messages.push(message);
+    this.last_msg = message.timeStamp;
+  }
+
+  pushUser(user: User){
+    this.users.push(user);
+  }
+  popUser(user: User){
+    this.users.splice(this.users.findIndex((u: User)=>{return user.equals(u)}), 1);
+  }
+}
+
+export class User {
+  public name: string;
+  readonly id: string;
+
+  constructor(name, id: string){
+    this.name = name;
+    this.id = id;
+  }
+
+  equals(user: User){
+    return user.id == this.id;
   }
 }
 
@@ -19,20 +66,30 @@ export class Message{
 })
 export class ChatComponent implements OnInit {
 
-  private selected: string = 'all';
+  public selected: string = 'global';
   private message: string;
   private alertMessage: string = "";
+  private positive: boolean;
   private messages: Message[] = [];
-  private chatrooms: any = [0, 1];
+  private chatrooms: any = {'global': new Chatroom('global', '0','group' )};
+
 
 
   constructor(private socketService: SocketService) {
-    this.socketService._socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
-      this.messages.push(msg);
+    this.socketService._socket.on('connected users', (users) => {
+      users.map((user) => {this.chatrooms.global.pushUser(user); console.log(user)});
     });
-    this.socketService._socket.on('user connected', (user) => {
-      this.sendAlert('user ' + user + ' connected')
+    this.socketService._socket.on('chat message', (msg) => {
+      console.log(msg);
+      this.chatrooms[msg.to].pushMessage(new Message(msg.message, msg.to, new Date(msg.timeStamp), msg.sender));
+    });
+    this.socketService._socket.on('user connected', (user, id) => {
+      this.chatrooms.global.pushUser(new User(user, id));
+      this.sendAlert('user ' + user + ' connected', true);
+    });
+    this.socketService._socket.on('user disconnected', (user, id) => {
+      this.chatrooms.global.popUser(new User(user, id));
+      this.sendAlert('user ' + user + ' disconnected', false)
     });
   }
 
@@ -40,15 +97,22 @@ export class ChatComponent implements OnInit {
   }
 
   onClickSend(){
-    var messageObj: Message = new Message(this.message, this.selected);
+    var messageObj: Message = new Message(this.message, this.selected, new Date());
     this.socketService.sendMessage(messageObj);
-    this.messages.push(messageObj);
+    this.chatrooms[this.selected].messages.push(messageObj);
     this.message = '';
   }
 
-  private sendAlert(msg){
-    setTimeout(() => this.alertMessage = null, 2000);
+  onClickCreateGroup(){
+
+  }
+
+  private sendAlert(msg: string, positive: boolean){
+    this.positive = positive;
     this.alertMessage = msg;
+    setTimeout(() => {
+      this.alertMessage = null;
+    }, 2000);
   }
 
 }
--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+let users= [];
 
 io.on('connection', function(socket){
     console.log('a user connected');
+    console.log(users);
     socket.username = socket.handshake.query.username;
     socket.broadcast.emit('user connected', socket.username);
+    socket.emit('connected users', users);
+    users.push({id: socket.id, name: socket.username});
+    console.log(users);
     socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
+
         msg.sender = socket.username;
+        console.log('message: ' + JSON.stringify(msg));
         socket.broadcast.emit('chat message', msg);
     });
     socket.on('disconnect', function(){
         console.log('user disconnected');
+        socket.broadcast.emit('user disconnected', socket.username, socket.id);
+        users.splice(users.findIndex((id)=>{return id == this.id}),1);
+        console.log(users);
     });
 });
 
