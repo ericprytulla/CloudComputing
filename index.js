38cc27a6b29e596bfa5b9e3a7d1d1ae8251ca3ae
EricPrytulla <eric.prytulla@dibuco.de>
2019-05-05 22:41:44 +0200

    register functionality

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ app.use(cors());
 app.use(express.static(html));
 app.use(bodyParser.json());
 
-app.use (function (req, res, next) {
+/**app.use (function (req, res, next) {
     if (req.secure || process.env.BLUEMIX_REGION === undefined) {
         next();
     } else {
         console.log('redirecting to https');
         res.redirect('https://' + req.headers.host + req.url);
     }
-});
+});**/
 
 
 io.on('connection', function(socket){
